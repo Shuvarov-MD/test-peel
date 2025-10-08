f6857b1a7d0032275ea3d3eb74c2fc147c4ce9bb
Mikhail Shuvarov <shuvarov.md@gmail.com>
2025-10-08 16:03:36 +0300

    add: добавлены скрипты, стили разделены на отдельные блоки

--- local/templates/peel3d/js/main.js
+++ local/templates/peel3d/js/main.js
@@ -1,4 +1,45 @@
-const mobileMenuToggle = document.querySelector('.site-header__toggle');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const toggles = document.querySelectorAll('.site-navigation__toggle');
+  toggles.forEach(function (toggle) {
+    toggle.addEventListener('click', function () {
+      const parentItem = toggle.closest('.site-navigation__item--children');
+      const sublist = parentItem.querySelector('.site-navigation__sublist');
+
+      // Закрыть все открытые подменю, кроме текущего
+      document.querySelectorAll('.site-navigation__sublist.open').forEach(function (openSublist) {
+        if (openSublist !== sublist) {
+          openSublist.classList.remove('open');
+        }
+      });
+
+      console.log(sublist);
+      // Переключить текущее подменю
+      sublist.classList.toggle('open');
+    });
+  });
+
+  const navigationToggles = document.querySelectorAll('.site-footer__navigation-toggle');
+  navigationToggles.forEach(function (toggle) {
+    toggle.addEventListener('click', function () {
+      const parentItem = toggle.closest('.site-footer__navigation-item--children');
+      const sublist = parentItem.querySelector('.site-footer__navigation-sublist');
+
+      // Закрыть все открытые подменю, кроме текущего
+      document.querySelectorAll('.site-footer__navigation-sublist.open').forEach(function (openSublist) {
+        if (openSublist !== sublist) {
+          openSublist.classList.remove('open');
+        }
+      });
+
+      // Переключить текущее подменю
+      sublist.classList.toggle('open');
+    });
+  });
+
+
+  const mobileMenuToggle = document.querySelector('.site-header__toggle');
 const mobileMenu = document.querySelector('.site-header__wrapper');
 const body = document.querySelector('body');
 
@@ -15,15 +56,52 @@ let swiperHero = new Swiper(".hero__swiper", {
       },
 });
 
-var swiper = new Swiper(".clients__swiper", {
-    slidesPerView: 4,
+let swiperClients = new Swiper(".clients__swiper", {
+    slidesPerView: 1,
     spaceBetween: 30,
     pagination: {
-        el: ".clients__swiper-pagination",
-        clickable: true,
+      el: ".clients__swiper-pagination",
+      clickable: true,
     },
     navigation: {
-        nextEl: ".clients__swiper-button--next",
-        prevEl: ".clients__swiper-button--prev",
+      nextEl: ".clients__swiper-button.swiper-button-next",
+      prevEl: ".clients__swiper-button.swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      576: {
+        slidesPerView: 2,
       },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 640px
+      992: {
+        slidesPerView: 4,
+      }
+    }
+});
+
+let swiperExamples = new Swiper(".examples__swiper", {
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    navigation: {
+      nextEl: ".examples__swiper-button.swiper-button-next",
+      prevEl: ".examples__swiper-button.swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    },
+});
+
+let swiperModelingSteps = new Swiper(".modeling-steps__slider", {
+  // direction: "vertical",
+  // mousewheel: true,
+});
 });
